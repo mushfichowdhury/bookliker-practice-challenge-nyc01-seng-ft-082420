a6ba0cd0c90be8457d179493aeae8d29b4956b95
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-10-11 21:58:18 -0400

    Working on debugging like button

--- js/index.js
+++ js/index.js
@@ -2,13 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const baseURL = "http://localhost:3000/books/"
     const currentUser = {"id":1, "username":"pouros"}
 
-
-// 1. getBooks --> fetch request for books
-// 2. renderBooks --> Display book title on div.list-panel
-// 3. .addEventListener to book title in order to display book details
-
-    
-
     const getBooks = () => {
         fetch(baseURL)
         .then(resp => resp.json())
@@ -28,21 +21,23 @@ document.addEventListener("DOMContentLoaded", function() {
         bookLi.classList.add("book-class")
         bookLi.dataset.id = bookObj.id 
 
-        bookLi.innerText = `${bookObj.title}`
-
+        bookLi.textContent = bookObj.title
         bookList.appendChild(bookLi)
         }
     
     const renderBookInfo = (clickedBook) => {
-        let showPanel = document.querySelector('#show-panel')
+        const showPanel = document.querySelector('#show-panel')
         showPanel.dataset.id = clickedBook.id 
         const bookUsers = clickedBook.users
-
+        const alreadyLiked = bookUsers.find(users => users.username === currentUser.username)
         
         showPanel.innerHTML = `
         <img src= ${clickedBook.img_url}>
-        <h3>${clickedBook.description}</h3>
-        <button class="like">Like</button>
+        <h2>${clickedBook.title}</h2>
+        <h2>${clickedBook.subtitle}</h2>
+        <h2>${clickedBook.author}</h2>
+        <p>${clickedBook.description}</p>
+        <h4>Likes</h4>
         `
         
         bookUsers.forEach(user => {
@@ -54,8 +49,54 @@ document.addEventListener("DOMContentLoaded", function() {
             showPanel.appendChild(bookUsersList)
             bookUsersList.appendChild(li)
         })
+
+        const likeBtn = document.createElement('button')
+        likeBtn.id = 'like-btn'
+        showPanel.append(likeBtn)
+        
+        if(!alreadyLiked) {
+            likeBtn.textContent = 'LIKE'
+        } 
+        else {
+            likeBtn.textContent = 'UNLIKE'
+        }
     }
 
+    const getLikes = (bookId) => {
+
+        fetch(baseURL + bookId)
+        .then(response => response.json())
+        .then(book => {
+            let usersLiked = book.users
+            const alreadyLiked = usersLiked.find(user => user.username === currentUser.username)
+            
+            if(!alreadyLiked) {
+                usersLiked.push(currentUser)
+                addLike(usersLiked, book.id)
+            } else {
+                usersLiked.pop(currentUser)
+                addLike(usersLiked, book.id)
+            }
+        })
+    }
+
+    const addLike = (usersLiked, book) => {
+        const options = {
+            method: "PATCH",
+            headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+            }, 
+            body: JSON.stringify({
+            users: usersLiked
+            })
+        }
+    
+        fetch(baseURL + book, options)
+            .then(response => response.json())
+            .then(clickedBook => renderBookInfo(clickedBook))
+        
+    }
     
     const clickHandler = () => {
         document.addEventListener('click', function(e) {
@@ -68,22 +109,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 .then(response => response.json())
                 .then(clickedBook => renderBookInfo(clickedBook))
             }
-
-            else if(e.target.matches(".like")) {
-                const likeBtn = e.target 
-                //assign likeBtn to the parent element ID
-
-                getLike(&&&)
+        })
+    }
+            
+    const likeHandler = () => {
+        document.addEventListener('click', function(e) {
+            if(e.target.id === "like-btn") {
+                const likeBtn = e.target
+                const bookId = likeBtn.parentElement.dataset.id
+                getLikes(bookId)
             }
         })
-
-
-
     }
     
- 
-    
+
     
     getBooks();
     clickHandler();
+    likeHandler();
 });
